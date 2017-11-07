32a4a38803ef41ab44edf63cd4e077077d25d801
Nela Smrzova <nelasmrzova@gmail.com>
2017-11-07 15:15:30 +0100

    piskvorky removed

--- piskvorky/ai.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-
-def rand_input(herni_pole):
-    prazdne_indexy = [idx for idx,i in enumerate(herni_pole) if i == '-']
-    return prazdne_indexy[randint(0,len(prazdne_indexy)-1)]
-
-def seq_input(herni_pole):
-    volna_pozice = herni_pole.index('-')
-    return volna_pozice
